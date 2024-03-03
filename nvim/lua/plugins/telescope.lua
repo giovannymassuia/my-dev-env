ac043a5c4d323e841b9a1fe4fa4dd3a4ad9ceb8d
Giovanny Massuia <giovannycmassuia@gmail.com>
2024-03-03 11:40:16 -0800

    telescope keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
 return {
+  { "nvim-telescope/telescope-live-grep-args.nvim" },
+  {
+    "aaronhallaert/advanced-git-search.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-fugitive",
+      "tpope/vim-rhubarb",
+    },
+  },
   {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
@@ -10,6 +19,7 @@ return {
     config = function()
       local telescope = require("telescope")
       local actions = require("telescope.actions")
+      local builtin = require("telescope.builtin")
 
       telescope.setup({
         defaults = {
@@ -31,22 +41,33 @@ return {
 
       keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
       keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-      keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-      keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" }) -- cwd -> current working directory
+      keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Open buffers" })
+      keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Show symbols in file" })
+      keymap.set("n", "<leader>fh", builtin.lsp_document_symbols, { desc = "Git options" })
+      keymap.set(
+        "n",
+        "<leader>fc",
+        "<cmd>Telescope grep_string<cr>",
+        { desc = "Find string under cursor in cwd" }
+      )
     end,
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-      require("telescope").setup {
+      require("telescope").setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
       require("telescope").load_extension("ui-select")
-    end
-  }
+      require("telescope").load_extension("advanced_git_search")
+      require("telescope").load_extension("live_grep_args")
+      -- require("telescope").load_extension("colors")
+      -- require("telescope").load_extension("noice")
+    end,
+  },
 }
