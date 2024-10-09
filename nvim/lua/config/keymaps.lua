bc0dcf4229bf80d6d727a82c6f7a34d140a9b6b6
Giovanny Massuia <giovannycmassuia@gmail.com>
2024-10-09 11:21:06 -0700

    enable back go forward

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -63,7 +63,7 @@ keymap.set("n", "sv", ":split<Return>", opts)
 
 -- Tabs
 keymap.set("n", "te", ":tabedit", opts)
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+-- keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- package-info keymaps
