4e94f89aa4c2b35e295f7c97c4c7f18a22c07be8
Giovanny Massuia <giovannycmassuia@gmail.com>
2024-10-09 11:44:33 -0700

    placeholder for neotest with playright

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,10 +1,15 @@
 return {
-    {
-      "nvim-neotest/neotest",
-      dependencies = { "nvim-neotest/neotest-jest", "marilari88/neotest-vitest" },
-      opts = function(_, opts)
-        table.insert(opts.adapters, require("neotest-jest"))
-        table.insert(opts.adapters, require("neotest-vitest"))
-      end,
-    },
-  }
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/neotest-jest",
+			"marilari88/neotest-vitest",
+			-- "thenbe/neotest-playwright",
+		},
+		opts = function(_, opts)
+			table.insert(opts.adapters, require("neotest-jest"))
+			table.insert(opts.adapters, require("neotest-vitest"))
+			-- table.insert(opts.adapters, require("neotest-playwright"))
+		end,
+	},
+}
